a7091f78cb8bbf890964bad5b937396341693353
Hend Jannat <h.jannat@lamah.com>
2024-05-20 15:58:41 +0200

    convert

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,9 +30,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import kotlin.math.pow
 
 class MainActivity : ComponentActivity() {
 
@@ -61,6 +64,16 @@ fun UnitConverter() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        var inputValue: Float? by remember {
+            mutableStateOf(null)
+        }
+        var result: Float? by remember {
+            mutableStateOf(null)
+        }
+        val unitsMap: Map<String, Float> = mapOf(
+            "mm" to 10F.pow(3), "cm" to 10F.pow(2),
+            "m" to 1F, "Km" to 10F.pow(-3)
+        )
         Text(
             text = "UnitConverter", modifier = Modifier.padding(
                 bottom = Dp
@@ -72,44 +85,63 @@ fun UnitConverter() {
                 bottom = Dp
                     (16f)
             ),
-            value = "", onValueChange = {
+            value = if (inputValue == null) "" else inputValue.toString(),
+            onValueChange
+            = {
+                inputValue = it.toFloat()
 
             })
         Row {
             val context = LocalContext.current
-            var isFromDropdownExpanded by remember {
+            var isFromDropdownExpanded: Boolean by remember {
                 mutableStateOf(false)
             }
-
-            fun setIsFromDropdownExpanded() {
-                println("setIsFromDropdownExpanded : $isFromDropdownExpanded")
-                isFromDropdownExpanded = !isFromDropdownExpanded
+            var fromUnit: String? by remember {
+                mutableStateOf(null)
             }
-
-
             var isToDropdownExpanded by remember {
                 mutableStateOf(false)
             }
+            var toUnit: String? by remember {
+                mutableStateOf(null)
+            }
 
-            fun setIsToDropdownExpanded() {
-                println("setIsFromDropdownExpanded clicked")
-                isToDropdownExpanded = !isToDropdownExpanded
+            fun convert() {
+                if (fromUnit != null && unitsMap[toUnit] != null && inputValue !=
+                    null
+                ) {
+                    val factor =
+                        unitsMap[toUnit]?.div(unitsMap[fromUnit]!!) as Float
+                    result = inputValue!! * factor
+                }
             }
             DropDown(
                 buttonText = "From",
                 isFromDropdownExpanded,
-                ::setIsFromDropdownExpanded
+                fun() {
+                    isFromDropdownExpanded = !isFromDropdownExpanded
+                },
+                fromUnit,
+                fun(unit: String) {
+                    fromUnit = unit
+                    convert()
+                }
             )
-
-
             Spacer(modifier = Modifier.width(Dp(10f)))
             DropDown(
                 buttonText = "To",
                 isToDropdownExpanded,
-                ::setIsToDropdownExpanded
+                fun() {
+                    isToDropdownExpanded = !isToDropdownExpanded
+                },
+                toUnit,
+                fun(unit: String) {
+                    toUnit = unit
+                    convert()
+                }
             )
         }
-        Text(text = "Result = ")
+        Text(text = "Result = ${if (result != null) result else ""} ")
 
     }
 }
@@ -117,8 +149,9 @@ fun UnitConverter() {
 @Composable
 fun DropDown(
     buttonText: String, isExpanded: Boolean,
-    setIsExpanded: () -> (Unit) = {}
-
+    setIsExpanded: () -> (Unit) = {},
+    selectedUnit: String?,
+    onSelectUnit: (String) -> (Unit) = {},
 ) {
 
     Box() {
@@ -137,10 +170,18 @@ fun DropDown(
             = {
                 setIsExpanded()
             }) {
-            listOf("cm", "m", "C", "F").map {
+            listOf("mm", "cm", "m", "Km").map {
                 DropdownMenuItem(
                     text = { Text(it) },
-                    onClick = { /*TODO*/ })
+                    onClick = { onSelectUnit(it) },
+                    colors = if (it == selectedUnit)
+                        MenuDefaults.itemColors(
+                            textColor =
+                            Color
+                                .Blue
+                        ) else MenuDefaults.itemColors()
+                )
+
             }
         }
     }
